6c221ddd815aaaeb7258ed4a1d1ad11d1b3159bc
Tolik Dem <tolik.demianov@gmail.com>
2022-07-29 23:50:42 +0300

    chore: Переменные окружения для TypeOrmModule
    Что сделано:
    
    1)
    В файл environment.ts добавил поле connection, которое содержит переменные окружения из терминала.
    
    На линуксе чтобы считать переменный окружения в терминал и любого файла нужно выполнить следующею команду
    
    `export $(grep -v '^#' <путь к файлу>`
    
    В моём случае полная команда запуска бекенд приложения с заданными переменными окружения выглядит вот так:
    ` export $(grep -v '^#' .env_configs/.env.development.local | xargs -d '\n') &&  yarn backend-api:start `
    
    2)
    В app.module.ts инициализация TypeOrmModule куда передаются как раз таки переменные окружения из файла environment.ts из поля connecion

--- apps/backend/api/src/environments/environment.ts
+++ apps/backend/api/src/environments/environment.ts
@@ -1,3 +1,29 @@
 export const environment = {
     production: false,
+    connection: {
+        type: process.env.DB_TYPE as 'aurora-postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USER_NAME,
+        password: process.env.DB_USER_PASSWORD,
+        database: process.env.DB_NAME,
+        dropSchema: false,
+        synchronize: true,
+        logging: true,
+        entities: [
+            'dist/apps/backend/api/src/**/*.entity{.ts, .js}',
+        ],
+        migrations: [
+            'dist/apps/backend/api/migrations/*{.ts, .js}',
+        ],
+        cli: {
+            'migrationsDir': 'db/migrations',
+        },
+    }
 };
+
+/*
+How to export environment variables from any file on Linux in terminal before running any app/utils
+export $(grep -v '^#' <path_to_env_file> | xargs -d '\n') &&  yarn backend-api:start
+export $(grep -v '^#' .env_configs/.env.development.local | xargs -d '\n') &&  yarn backend-api:start
+*/
