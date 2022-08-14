822ca7d65563909e1505460e3a0e213c3b7726df
Tolik Dem <tolik.demianov@gmail.com>
2022-08-14 14:26:37 +0300

    chore: GraphQl - базовая конфигурация
    Пришлось установить `@nestjs/apollo` потому что в последней версии `@nestjs/graphql` его создатели решили разделить на несколько отдельных библиотек.
    
    In the latest version, we made a decision to break the @nestjs/graphql package up into a few separate libraries, letting you choose whether to use Apollo (@nestjs/apollo), Mercurius (@nestjs/mercurius), or another GraphQL library in your project.
    
    [See more](https://docs.nestjs.com/graphql/migration-guide)

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { environment } from '../environments/environment';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppResolver } from './app.resolver';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
     imports: [
         TypeOrmModule.forRoot({
             ...environment.connection,
         }),
+        GraphQLModule.forRoot({
+            typePaths: ['./**/*.graphql'],
+            context: ({req}) => ({req}),
+            playground: true,
+            driver: ApolloDriver,
+        }),
     ],
     controllers: [AppController],
     providers: [
-
+        AppResolver,
     ],
 })
 export class AppModule {}
